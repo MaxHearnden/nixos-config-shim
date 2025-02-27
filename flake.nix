1a44e82039e5c9dd8558509ca8e6388e163e5944
MaxHearnden <maxoscarhearnden@gmail.com>
2025-02-27 17:00:29 +0000

    Revert "ignore unused inputs"
    This reverts commit f412c4ce7fee622bcbe0fd77da79229559d05b0f.
    
    It's no longer required after 5fd8106dcce23d8b84ddee7dc4d629990c65b854

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     url = "github:MaxHearnden/nixos-config";
   };
 
-  outputs = { config, self, ... }: {
+  outputs = { config, self }: {
     inherit (config) packages hydraJobs nixosConfigurations;
   };
 }
