c1a7b43f660e91bbf0b455c58cc7c03b57a4c54e
MaxHearnden <maxoscarhearnden@gmail.com>
2025-02-27 16:11:24 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    config = {
+      inputs = {
+        nixos-kexec.follows = "nixos-kexec";
+        latest-system.follows = "latest-system";
+      };
+      url = "github:MaxHearnden/nixos-config";
+    };
+
+    latest-system = {
+      inputs.nixpkgs.follows = "config/nixpkgs";
+      url = "github:MaxHearnden/latest-system";
+    };
+
+    nixos-kexec = {
+      inputs.nixpkgs.follows = "config/nixpkgs";
+      url = "github:MaxHearnden/nixos-kexec";
+    };
+  };
+
+  outputs = { config, self }: {
+    inherit (config) packages hydraJobs nixosConfigurations;
+  };
+}
